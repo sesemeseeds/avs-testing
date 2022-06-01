751a56ea4e146cbc7d481ba605155e4b4d1c2f43
Emilio Sese <emilio.sese@fisglobal.com>
2022-06-01 10:55:48 -0400

    rearranged library and added new ui from fis ruf

---
+++ ui-application/apps/ui-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'avs-test-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ui-app';
+}
